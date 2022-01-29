9056b005cbd1384b4076f36c22585be9b2b35861
zuri227 <zuri22780@gmail.com>
2022-01-29 18:50:46 -0500

    Work through task 3

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  prevGuesses: [],
+  getGuess: function(){
+    console.log("Enter a guess between ${this.smallestNum} and ${this.biggestNum}")
+    let guess = 0;
+    while (guess < biggestNum && guess > smallestNum){
+    }
+    return guess;
+  },
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
   }
 }
+
