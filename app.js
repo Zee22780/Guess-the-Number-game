41b8bf500257435283b9cd95857748bdba956425
zuri227 <zuri22780@gmail.com>
2022-01-30 16:22:45 -0500

    Work on task play function

--- app.js
+++ app.js
@@ -4,21 +4,24 @@ const game = {
   smallestNum: 1,
   secretNum: null,
   prevGuesses: [],
-  playerGuess: null,
-  getGuess: function(){
-    console.log(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`)
-    while (this.playerGuess < this.biggestNum && this.playerGuess > this.smallestNum && typeof this.playerGuess === "number"){
-      return this.playerGuess;
-    }
-  },
   play: function() {
+  //Generates a random number and assigns it to the variable secretNum
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+  //Takes the guesses from the player and pushes them to the prevGuesses array while the last guess is NOT equal to secretNum (the random number chosen by computer).
+    do {
+      this.prevGuesses.push(this.getGuess());
+    } while (this.prevGuesses[this.prevGuesses.length-1] !== this.secretNum)
+  },
+  //Prompts player to guess and stores that value in playerGuess.
+  getGuess: function() {
+    let playerGuess 
 
-    while () {
-    game.getGuess();
+    do {
+      playerGuess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`));
     }
-    this.prevGuesses.push([]);
-      }
-  }
+    while (isNan(playerGuess) || playerGuess >this.biggestNum || playerGuess <this.smallestNum) 
 
+    return playerGuess;
+  }
+}
